892b8396c079d71655dc28022da7eb8d2f985818
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-05 21:41:34 +0000

    automatically backed up by learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -16,5 +16,8 @@ class Users extends Component {
 }
 
 //add mapStateToProps here
+const mapStateToProps = (state) => {
+  return { users: state.users, count: state.users.length }
+}
 
 export default Users
